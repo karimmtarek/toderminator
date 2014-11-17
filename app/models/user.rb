58da8b9771edfbde730efbc9cb9a88d66f88b776
Karim Tarek <karimmtarek@gmail.com>
2014-11-17 10:52:55 -0500

    hacking devise to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_many :lists, dependent: :destroy
   # do I need to add 'has_many :todo_items' as well?
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
