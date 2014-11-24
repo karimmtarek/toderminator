c94e15c066ee70b2ccd6e9757195027ac134e550
Karim Tarek <karimmtarek@gmail.com>
2014-11-23 23:33:16 -0500

    deleted useless migrations

--- db/migrate/20141117054221_add_field_to_list.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldToList < ActiveRecord::Migration
-  def change
-    add_reference :lists, :User, index: true
-  end
-end
--- db/migrate/20141123041801_remove_user_from_list.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromList < ActiveRecord::Migration
-  def change
-    remove_column :lists, :User_id, :integer
-  end
-end
